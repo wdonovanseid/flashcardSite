b82ae175ea56f0dca943711eadcd1ee27cf84fc3
drewparker <drewjparker91@gmail.com>
2020-08-11 15:46:04 -0700

    Add clickables to all js, jq terms
    Co-Authored-by: William DS <wdstwo@live.com>

---
+++ Desktop/flashcardSite/README.md
@@ -0,0 +1,34 @@
+# _Flashcard Website_
+
+#### _Epicodus Exercise Flashcard, 8/11/2020_
+
+#### By _**Drew Parker and William Donovan-Seid**_
+
+## Description
+
+_This is a practice exercise using Javascript to make a flashcard._
+
+## Setup/Installation Requirements
+
+* _Copy Repository_
+* _Open index.html in your web browser_
+
+## Known Bugs
+
+_There are no known bugs at this time._
+
+## Support and contact details
+
+_If there are any questions or concerns, feel free to contact us at drewparker91@gmail.com and wdstwo@live.com._
+
+## Technologies Used
+
+* HTML
+* CSS
+* Javascript
+
+### License
+
+This software is licensed under the MIT license.
+
+Copyright (c) 2020 **_Drew Parker and William Donovan-Seid_**
--- Desktop/flashcardSite/js/scripts.js
+++ Desktop/flashcardSite/js/scripts.js
@@ -3,12 +3,57 @@ $(document).ready(function() {
     $("#jsShow").toggle();
     $("#jsHidden").toggle();
   });
+
   $(".clickable1").click(function() {
-    $("#germany-showing").slideToggle();
-    $("#germany-hidden").slideToggle();
+    $("#operShow").toggle();
+    $("#operHidden").toggle();
   });
+
   $(".clickable2").click(function() {
-    $("#netherlands-showing").slideToggle();
-    $("#netherlands-hidden").slideToggle();
+    $("#variShow").toggle();
+    $("#variHidden").toggle();
   });
+
+  $(".clickable3").click(function() {
+    $("#variNameShow").toggle();
+    $("#variNameHidden").toggle();
+  });
+
+  $(".clickable4").click(function() {
+    $("#funcShow").toggle();
+    $("#funcHidden").toggle();
+  });
+
+  $(".clickable5").click(function() {
+    $("#methShow").toggle();
+    $("#methHidden").toggle();
+  });
+
+  $(".clickable6").click(function() {
+    $("#argShow").toggle();
+    $("#argHidden").toggle();
+  });
+
+  $(".clickable7").click(function() {
+    $("#paraShow").toggle();
+    $("#paraHidden").toggle();
+  });
+
+  $(".clickable8").click(function() {
+    $("#retShow").toggle();
+    $("#retHidden").toggle();
+  });
+
+  $(".clickable9").click(function() {
+    $("#chainShow").toggle();
+    $("#chainHidden").toggle();
+  });
+
+  $(".clickable10").click(function() {
+    $("#stringShow").toggle();
+    $("#stringHidden").toggle();
+  });
+
+  
+
 });
